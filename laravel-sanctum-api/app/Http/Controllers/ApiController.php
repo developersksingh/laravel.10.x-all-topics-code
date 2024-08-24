58b88737d50b9f8f2437a413a61ef6cfc3612844
developersksingh <sanjayletuscode@gmail.com>
2024-08-24 15:15:49 +0530

    User Login API using Sanctum
    User Login API using Sanctum
    
      // Login API (POST, FormData)
        public function login(Request $request)
        {
            // Data Validations
            $request->validate([
                "email" => "required|email",
                "password" => "required"
            ]);
            //Email Check : In Databse
            $userInfo  = User::where(["email" => $request->email])->first();
    
            // If user Exist than concept of password validation
            if (!empty($userInfo)) {
                if (Hash::check($request->password, $userInfo->password)) {
                    // User Exists
                    $accessToken  =  $userInfo->createToken("myToken")->plainTextToken;
                    return response()->json([[
                        "status" => true,
                        "message" => "Login Successfull",
                        "accessToken" => $accessToken
                    ]]);
    
                }else{
                    return response()->json([[
                        "status" => false,
                        "message" => "password didn't match"
                    ]]);
                }
    
                // Sanctum Token Value
            }else{
                return response()->json([[
                    "status" => false,
                    "message" => "Invalid login credentials"
                ]]);
            }
        }

--- laravel-sanctum-api/app/Http/Controllers/ApiController.php
+++ laravel-sanctum-api/app/Http/Controllers/ApiController.php
@@ -6,23 +6,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
-class ApiController extends Controller{
+class ApiController extends Controller
+{
 
     //Register API (POST,FormData)
-    public function register(Request $request){
+    public function register(Request $request)
+    {
 
         // Data Validations
         $request->validate([
-          "name" => "required|string",
-          "email" => "required|email|unique:users",
-          "password" => "required|confirmed"
+            "name" => "required|string",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
         ]);
 
         //Create a user
         user::create([
-            "name"=>$request->name,
-            "email"=>$request->email,
-            "password"=>Hash::make($request->password)
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
         ]);
         return response()->json([
             "status" => true,
@@ -31,17 +33,46 @@ class ApiController extends Controller{
     }
 
     // Login API (POST, FormData)
-    public function login(Request $request){
+    public function login(Request $request)
+    {
+        // Data Validations
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        //Email Check : In Databse
+        $userInfo  = User::where(["email" => $request->email])->first();
 
+        // If user Exist than concept of password validation
+        if (!empty($userInfo)) {
+            if (Hash::check($request->password, $userInfo->password)) {
+                // User Exists
+                $accessToken  =  $userInfo->createToken("myToken")->plainTextToken;
+                return response()->json([[
+                    "status" => true,
+                    "message" => "Login Successfull",
+                    "accessToken" => $accessToken
+                ]]);
+
+            }else{
+                return response()->json([[
+                    "status" => false,
+                    "message" => "password didn't match"
+                ]]);
+            }
+
+            // Sanctum Token Value
+        }else{
+            return response()->json([[
+                "status" => false,
+                "message" => "Invalid login credentials"
+            ]]);
+        }
     }
 
     // Profile API (GET)
-    public function profile(){
-
-    }
+    public function profile() {}
 
     // Logout API (GET)
-    public function logout(){
-
-    }
+    public function logout() {}
 }
