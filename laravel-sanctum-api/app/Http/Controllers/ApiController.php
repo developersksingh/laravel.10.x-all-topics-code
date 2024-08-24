a5e59e1886a44e31771679ed6fef159c30595aaa
developersksingh <sanjayletuscode@gmail.com>
2024-08-24 13:44:58 +0530

    API Controllers & MethodS Settings
     API Controllers & Methods Settings

---
+++ laravel-sanctum-api/app/Http/Controllers/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller{
+
+    //Register API (POST,FormData)
+    public function register(Request $request){
+
+    }
+
+    // Login API (POST, FormData)
+    public function login(Request $request){
+
+    }
+
+    // Profile API (GET)
+    public function profile(){
+
+    }
+
+    // Logout API (GET)
+    public function logout(){
+
+    }
+}
