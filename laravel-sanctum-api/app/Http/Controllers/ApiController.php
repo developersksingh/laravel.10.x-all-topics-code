084159d7b68ba041b5f9a572aa816e69c7c00a62
developersksingh <sanjayletuscode@gmail.com>
2024-08-24 14:32:33 +0530

    For User Register API Code
    For User Register API Code

--- laravel-sanctum-api/app/Http/Controllers/ApiController.php
+++ laravel-sanctum-api/app/Http/Controllers/ApiController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class ApiController extends Controller{
 
     //Register API (POST,FormData)
     public function register(Request $request){
+
+        // Data Validations
         $request->validate([
-          "name" => "required|max:100",
+          "name" => "required|string",
           "email" => "required|email|unique:users",
           "password" => "required|confirmed"
+        ]);
+
+        //Create a user
+        user::create([
+            "name"=>$request->name,
+            "email"=>$request->email,
+            "password"=>Hash::make($request->password)
+        ]);
+        return response()->json([
+            "status" => true,
+            "message" =>  "User Registered Successfuly"
         ]);
     }
 
